7d904735bfff8b066e30886ca7245d634b06fec7
Jose Manuel Ganchozo <jganchozo@outlook.com>
2023-05-05 17:58:13 -0400

    elements and generic page

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
 const port = 8080;
 
@@ -7,23 +9,38 @@ const port = 8080;
 //     extensions: ['html']
 // }));
 
+//Handlebar
 app.set('view engine', 'hbs');
+hbs.registerPartials(`${__dirname}/views/partials`, (err) => { });
 
 // servir contenido estatico
 app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {
+        nombre: 'Jose Manuel',
+        titulo: 'Curso de Node'
+    });
 });
 
 app.get('/generic', (req, res) => {
-    res.sendFile(`${__dirname}/public/generic.html`);
+    res.render('generic', {
+        nombre: 'Jose Manuel',
+        titulo: 'Curso de Node'
+    });
 });
 
 app.get('/elements', (req, res) => {
-    res.sendFile(`${__dirname}/public/elements.html`);
+    res.render('elements', {
+        nombre: 'Jose Manuel',
+        titulo: 'Curso de Node'
+    });
 });
+
+// app.get('/elements', (req, res) => {
+//     res.sendFile(`${__dirname}/public/elements.html`);
+// });
 
 app.get('*', (req, res) => {
     res.sendFile(`${__dirname}/public/404.html`);
