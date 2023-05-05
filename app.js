2fbf3821f2cdd21b2957ec2315c543f18123d0d3
Jose Manuel Ganchozo <jganchozo@outlook.com>
2023-05-05 17:03:22 -0400

    adding hbs

--- app.js
+++ app.js
@@ -2,19 +2,31 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
+// servir contenido estatico
+// app.use(express.static('public', {
+//     extensions: ['html']
+// }));
 
-    res.send('Hello World');
+app.set('view engine', 'hbs');
+
+// servir contenido estatico
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
-app.get('/hello', (req, res) => {
+app.get('/generic', (req, res) => {
+    res.sendFile(`${__dirname}/public/generic.html`);
+});
 
-    res.send('Hello Route');
+app.get('/elements', (req, res) => {
+    res.sendFile(`${__dirname}/public/elements.html`);
 });
 
 app.get('*', (req, res) => {
-
-    res.send('404 | Page not found');
+    res.sendFile(`${__dirname}/public/404.html`);
 });
 
 app.listen(port, () => {
